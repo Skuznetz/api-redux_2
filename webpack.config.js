16d985a441f90cca245d84180322bc7abc8c05eb
Skuznetz <s.kuznetzov@mail.ru>
2018-08-18 21:00:30 +0400

    создадим файл модулей

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['react-hot', 'babel']
+                use: ['react-hot-loader', 'babel']
             },
             {
                 test: /\.less$/,
