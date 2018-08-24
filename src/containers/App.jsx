60cd6edd8df23068e9112197b2b88589ee43f321
Skuznetz <s.kuznetzov@mail.ru>
2018-08-24 19:23:12 +0400

    напишем импорт

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
 
+import { fetchUsers } from '../actions';
 class AppContainer extends Component {
     render() {
         return <h1>Привет</h1>;
