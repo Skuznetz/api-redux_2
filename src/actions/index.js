b88244130a6328be3bf61ab53250ab7f59b3e4a2
Skuznetz <s.kuznetzov@mail.ru>
2018-08-25 16:47:27 +0400

    dispatch REQUEST_USERS

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,9 @@ export const REQUEST_USERS = 'REQUEST_USERS';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 
 export const fetchUsers = language =>dispatch => {
+    dispatch({
+        type: REQUEST_USERS
+    })
     return api.getPopularUsersByLanguage(language)
     .then(data => dispatch({
         type: RECEIVE_USERS,
