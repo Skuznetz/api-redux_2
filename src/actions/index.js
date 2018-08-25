419ee178176ad8005cad7d6f3e38a7551f173028
Skuznetz <s.kuznetzov@mail.ru>
2018-08-25 09:57:41 +0400

    напишем константы

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import api from '../api';
 
+export const REQUEST_USERS = 'REQUEST_USERS';
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+
 export const fetchUsers = language =>dispatch => {
     return api.getPopularUsersByLanguage(language)
     .then(data => console.log(data));
