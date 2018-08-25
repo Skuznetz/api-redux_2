4bbb787e489c1e9536955b9c5de12c55295c1ecf
Skuznetz <s.kuznetzov@mail.ru>
2018-08-25 10:14:56 +0400

    обработаем экшн

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,13 @@
+const users = (state = {isFetching: false,items: [] }) =>{
+    switch (action.type){
+        case RECEIVE_USERS: {
+            return {
+                ...state,
+                isFetching:false,
+                items: action.users
+            }
+        }
+    }
+}
+
 export default (state = {},action) => state;
