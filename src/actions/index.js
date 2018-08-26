35afbe4e537f9fcd2dc33f14470d1182a6e609a4
Skuznetz <s.kuznetzov@mail.ru>
2018-08-26 09:34:34 +0400

    selectLanguage

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,10 @@ export const REQUEST_USERS = 'REQUEST_USERS';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const SELECT_LANGUAGE = 'SELECT_LANGUAGE';
 
+export const selectLanguage = language =>({
+    type: SELECT_LANGUAGE,
+    language
+});
 
 export const fetchUsers = language =>dispatch => {
     dispatch({
