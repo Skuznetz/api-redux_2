8934b2757b42d6e481878523b9e3dedbd8fa3c66
Skuznetz <s.kuznetzov@mail.ru>
2018-08-26 09:30:04 +0400

    напишем export

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import api from '../api';
 
 export const REQUEST_USERS = 'REQUEST_USERS';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const SELECT_LANGUAGE = 'SELECT_LANGUAGE';
+
 
 export const fetchUsers = language =>dispatch => {
     dispatch({
